b3d9d13fd5ac411a772b1485d7201d37e7be0306
GitHub User Name <rom644@gmail.com>
2021-07-04 14:32:05 +0300

    npm fix.

--- src/components/Info/EmotionsInfo.js
+++ src/components/Info/EmotionsInfo.js
@@ -1,14 +1,5 @@
 import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Grid from '@material-ui/core/Grid';
-
 import useStyles from './styles';
-import { Typography } from '@material-ui/core';
 import Modal from '../modal/Modal';
 
 const EmotionsInfo = ({ posts }) => {
